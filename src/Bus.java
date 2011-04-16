c12ad1993ee9beacf4b803aab68fbc293accdf6c
Travis Throne <travisthrone@gmail.com>
2011-04-16 02:38:21 -0500

    HAHA

--- src/Bus.java
+++ src/Bus.java
@@ -10,7 +10,7 @@ public class Bus {
         firstStop = start;                   //Integer in range [1, 20] determines which stop the bus starts at
         currentStop = start;                 //Keeps track of which stop the bus at, -1 if not stopped  
         nextStop = (currentStop%20) + 1;     //Finds next stop without a conditional
-
+	//GAY
     }
 
     public int getRiders() {
