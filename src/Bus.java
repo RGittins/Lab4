a495377a2ae99c60f0260bfbdbb959faf0826fce
Ryan Gittins <Ryan.Gittins@gmail.com>
2011-04-16 02:35:12 -0500

    Starting Point

---
+++ src/Bus.java
@@ -0,0 +1,27 @@
+
+public class Bus {
+
+    private int firstStop;
+    private int currentStop;
+    private int nextStop;
+    private int riders;
+
+    public Bus(int start, int riders) {
+        firstStop = start;                   //Integer in range [1, 20] determines which stop the bus starts at
+        currentStop = start;                 //Keeps track of which stop the bus at, -1 if not stopped  
+        nextStop = (currentStop%20) + 1;     //Finds next stop without a conditional
+
+    }
+
+    public int getRiders() {
+        return riders;
+    }
+  
+    public int getNextStop() {
+        return nextStop;
+    }
+
+    public int getCurrentStop() {
+    	return currentStop;
+    }
+}
